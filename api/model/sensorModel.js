8276544e3b1e30303763a4d9a8f6052360539fb8
Aravinthan-cmd <aravinthan.m85@gmail.com>
2024-03-18 13:29:02 +0530

    main

---
+++ api/model/sensorModel.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+const Data = new mongoose.Schema({
+    density: {
+        type:String
+    },
+    viscosity: {
+        type:String
+    },
+    temperature:  {
+        type:String
+    },
+    dtn:  {
+        type:String
+    },
+    oil: {
+        type:String
+    },
+    currentTemperature: {
+        type:String
+    },
+    TDT: {
+        type:String
+    },
+    TOC: {
+        type:String
+    }
+},{timestamps:true})
+export default mongoose.model("insertdata", Data);
